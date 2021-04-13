333609615d211874c431fbae3c2174659774698c
Konstantin-174 <timokhin_74@hotmail.com>
2021-04-13 22:42:00 +0500

    3st commit (Header)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import common from '../../styles/Common.module.scss';
+import local from './Header.module.scss';
 
 const Header = () => {
     return (
-        <header>
-            FIRST SCREEN
+        <header className={`${common.sectionOuter} + ${local.headerWrapper}`}>
+           <div className={`${common.sectionInner} + ${local.sectionInner}`}>
+               <div className={local.headerNav}>
+                   <div className={local.headerContacts}>
+                       <div className={local.headerContactsItem}>
+                           Новокузнечный переулок 4/1
+                       </div>
+                       <div className={local.headerContactsItem}>
+                           8 (812) 123-45-67
+                       </div>
+                   </div>
+                   <div className={local.headerLinks}>
+                       <div className={local.headerLinksItem}>
+                           <a href="#">Каталог</a>
+                       </div>
+                       <div className={local.headerLinksItem}>
+                           <a href="#">Доставка</a>
+                       </div>
+                       <div className={local.headerLinksItem}>
+                           <a href="#">Коллекции</a>
+                       </div>
+                       <div className={local.headerLinksItem}>
+                           <a href="#">Контакты</a>
+                       </div>
+                   </div>
+               </div>
+               <div className={local.headerBtns}>
+                   <button className={local.btnItem}>
+                       Винная карта
+                   </button>
+                   <button className={local.btnItem}>
+                       Дегустация
+                   </button>
+               </div>
+           </div>
         </header>
     )
 }
