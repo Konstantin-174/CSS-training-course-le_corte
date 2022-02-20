6c368acb60e067c8e009139d315c275ad1d08d0e
Konstantin-174 <timokhin_74@hotmail.com>
2022-02-20 13:27:43 +0500

    fix: fix bugs with loading page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.scss';
 import Header from './components/header/Header';
 import Text from './components/text/Text';
 import Collection from './components/collection/Collection';
@@ -7,15 +6,15 @@ import Form from './components/form/Form';
 import Footer from './components/footer/Footer';
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <Text/>
-            <Collection/>
-            <Form/>
-            <Footer/>
-        </div>
-    );
+  return (
+      <div className="App">
+        <Header/>
+        <Text/>
+        <Collection/>
+        <Form/>
+        <Footer/>
+      </div>
+  );
 }
 
 export default App;
